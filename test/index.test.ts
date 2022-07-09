ee08e29a8b0e189c02bd31488be0c7530ab91043
DVitaliy <dynnik.vitaliy@gmail.com>
2022-07-09 23:39:17 +0300

    Increase tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,7 @@
 import { getPaginationRange } from "../src"
 
-describe("blah", () => {
-  it("works", () => {
-    expect(getPaginationRange({ total: 8, current: 7 })).toEqual([
-      4, 5, 6, 7, 8,
-    ])
-  })
+it("Tests", () => {
+  expect(getPaginationRange({ total: 3, current: 1 })).toEqual([1, 2, 3])
+  expect(getPaginationRange({ total: 8, current: 7 })).toEqual([4, 5, 6, 7, 8])
+  expect(getPaginationRange({ total: 5, current: 3 })).toEqual([1, 2, 3, 4, 5])
 })
