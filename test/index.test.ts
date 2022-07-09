61fa0872b4736f1b7f00a14e01743b1d4fd1e88b
DVitaliy <dynnik.vitaliy@gmail.com>
2022-07-09 22:30:53 +0300

    Fixed test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,9 @@
-import getPagination from "../src"
+import { getPaginationRange } from "../src"
 
 describe("blah", () => {
   it("works", () => {
-    expect(getPagination({ total: 8, current: 7 })).toEqual([4, 5, 6, 7, 8])
+    expect(getPaginationRange({ total: 8, current: 7 })).toEqual([
+      4, 5, 6, 7, 8,
+    ])
   })
 })
